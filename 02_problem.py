370387c609e4f3c95af071035d517ceb3f260915
Ismail Sekh <ismailsekh2880@gmail.com>
2025-11-29 18:57:14 +0530

    Add files via upload

---
+++ 02_problem.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("The club isn’t the best place to find a lover So the bar is where I go Me and my friends at the table doing shots drinking fast and then we talk slow you come over and start up a conversation with just me and trust me I’ll give it a chance now Take my hand stop, put van the man on the jukebox and then we start to dance And now I’m singing like")
+engine.runAndWait()
